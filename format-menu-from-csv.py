4296f69af58738272e6d880cd8d47509d9e4a4a1
Christine McClure <camcclure@gmail.com>
2015-02-09 12:29:03 -0600

    added logic to check for non-null in cols 2-4

--- format-menu-from-csv.py
+++ format-menu-from-csv.py
@@ -7,6 +7,7 @@ print '''
 '''
 
 inFile = raw_input("\nEnter input filename to continue or <enter> to quit:\n")
+propFlag=0
 if len(inFile) < 1:
     print '\nProgram stopped'
 else: 
@@ -17,17 +18,28 @@ else:
       output = open(outFile,'w')
       with open(inFile) as input:
           line = csv.reader(input, delimiter=",")
+          i=0;
           for cols in line:
             if cols[0][:1]=='#':
                 print "\nHeader line found. Skipping this line: \n", cols
                 continue
  
-            if 'column' in cols[0]:  
-              print "Adding class for row ", val0
-              row = cols[0] + " " + "{\"url\":\"" + cols[1] + "\", \"options\":{\"attributes\":{\"class\":[\"" + 'iit-gh-menu-grid-4' + "\"]}}}\n"
-            else:
-              row = cols[0] + " " + "{\"url\":\"" + cols[1] + "\"}\n"
+#            if 'column' in cols[0]:  
+#              print "Adding class for row ", val0
+#              row = cols[0] + " " + "{\"url\":\"" + cols[1] + "\", \"options\":{\"attributes\":{\"class\":[\"" + 'iit-gh-menu-grid-4' + "\"]}}}\n"
+#            else:
+            propFlag=0
+            
+            if cols[2]!="":
+                propFlag=1
+            if cols[3]!="":
+                propFlag=1
+            if cols[4]!= "":
+                propFlag=1                
 
+            print "print row ** %s ** \t propFlag=%d" % (cols[0], propFlag)                
+            strPropFlag = str(propFlag)    
+            row = cols[0] + " " + "{\"url\":\"" + cols[1] + "\"}\n"
             row = re.sub('/', '\/', row)
             output.write(row)
             
